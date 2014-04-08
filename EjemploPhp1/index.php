1c76c6918d5f92a0b1e87d9a19e2f0de02826787
alumno <alumno@A03CLI13>
2014-04-08 11:53:33 +0200

    a

--- EjemploPhp1/index.php
+++ EjemploPhp1/index.php
@@ -4,16 +4,67 @@
         <title></title>
     </head>
     <body bgcolor="grey">
-        <a href="./03/prueba3.html">03 -UTILIZANDO FORMULARIOS CON PHP</a>
-        <br/>
-        <a href="./05/index.php">05 -CONEXIÓN PHP CON MYSQL</a>
-        <br/>
-        <a href="./06/index.php">06 -CONSULTAS DE SELECCIÓN CON PHP_MYSQL</a>
-        <br/>
-        <a href="./07/index.php">07 -FILTROS CON CONSULTAS DE SELECCIÓN CON PHP Y MYSQL</a>
-        <br/>
-        <a href="./08/index.php">08 -PAGINACIÓN DE REGISTROS CON PHP Y MYSQL</a>
-        <br/>
-        <a href="./09/index.php">09 -SUBIDA DE FICHEROS AL SERVIDOR</a>
+        <ul>
+            <li>
+                <h3>PHP</h3>
+                <a href="./03/prueba3.html">03 -UTILIZANDO FORMULARIOS CON PHP</a>
+                <br/>
+                <a href="./05/index.php">05 -CONEXIÓN PHP CON MYSQL</a>
+                <br/>
+                <a href="./06/index.php">06 -CONSULTAS DE SELECCIÓN CON PHP_MYSQL</a>
+                <br/>
+                <a href="./07/index.php">07 -FILTROS CON CONSULTAS DE SELECCIÓN CON PHP Y MYSQL</a>
+                <br/>
+                <a href="./08/index.php">08 -PAGINACIÓN DE REGISTROS CON PHP Y MYSQL</a>
+                <br/>
+                <a href="./09/index.php">09 -SUBIDA DE FICHEROS AL SERVIDOR</a>
+            </li>
+            <li>
+                <h3>HTML5 y CSS3</h3>
+                <ul>
+                    <li>
+                        <h3>HTML5</h3>
+                        <a href="./html5/Ejemplo2.html">02 - Nuevas etiquetas(header,footer,aside,nav,article,section)</a>
+                        <br/>
+                        <a href="./html5/Ejemplo3.html">03 - Hgroup</a>
+                        <br/>
+                        <a href="./html5/Ejemplo4.html">04 - Figure y Figcaption</a>
+                        <br/>
+                        05 - <a href="./html5/Ejemplo5.1.html">mark</a> <a href="./html5/Ejemplo5.2.html">progress</a>
+                        <br/>
+                        <a href="./html5/Ejemplo6.html">06 - Input, Placeholder, Email, Url, Date, Autofocus, Required</a>
+                        <br/>
+                        <a href="./html5/Ejemplo7.html">07 - Input type="range"</a>
+                        <br/>
+                        <a href="./html5/Ejemplo8.html">08 - DataList</a>
+                        <br/>
+                        <a href="./html5/Ejemplo9.html">09 - Video</a>
+                        <br/>
+                        <a href="./html5/Ejemplo10.html">10 - Output</a>
+                        <br/>
+                        <a href="./html5/Ejemplo11.html">11 - Audio</a>
+                        <br/>
+                        <a href="./html5/Ejemplo12.html">12 - Almacenamiento Web</a>
+                        <br/>
+                        <a href="./html5/Ejemplo13.html">13 - Drag&Drop</a>
+                        <br/>
+                        <a href="./html5/Ejemplo14.html">14 - GeoLocalización</a>
+                    </li>
+                    <li>
+                        <h3>CSS3</h3>
+                        <a href="./css3/Ejemplo15.html">15 - Transparencia</a>
+                        <br/>
+                        <a href="./css3/Ejemplo16.html">16 - Bordes redondeados</a>
+                        <br/>
+                        <a href="./css3/Ejemplo17.html">17 - Sombra en texto</a>
+                        <br/>
+                        <a href="./css3/Ejemplo18.html">18 - Multiples fondos</a>
+                        <br/>
+                        <a href="./css3/Ejemplo19.html">19 - Rotación de elementos</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        
     </body>
 </html>
